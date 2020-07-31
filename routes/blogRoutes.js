9f1bc3e5d2dde3cf18c0702f63268dd4441e7c2b
Taiga <taiga248@gmail.com>
2020-07-31 23:35:40 +0900

    routing処理をControllerに移動

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,53 +1,11 @@
 const express = require("express");
-const Blog = require("../models/blogs");
+const blogController = require("../controllers/blogCntroller");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then(result => {
-      res.render("index", { title: "All Blogs", blogs: result });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-router.post("/", (req, res) => {
-  const blog = new Blog(req.body);
-  blog
-    .save()
-    .then(result => {
-      res.redirect("/blogs");
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-// 詳細表示、削除の実装より下にに書いちゃだめ 遷移しなくなる
-router.get("/create", (req, res) => {
-  res.render("create", { title: "Create a new blog" });
-});
-
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then(result => {
-      res.render("details", { blog: result, title: "Blog details" });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then(result => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch(err => console.log(err));
-});
+router.get("/", blogController.blog_index);
+router.post("/", blogController.blog_create_post);
+router.get("/create", blogController.blog_create_get);
+router.get("/:id", blogController.blog_details);
+router.delete("/:id", blogController.blog_delete);
 
 module.exports = router;
